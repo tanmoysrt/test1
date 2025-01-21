e4cefd27eeb6bda080b7fded99f19b190021386b
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-01-21 16:25:53 +0530

    update

---
+++ test1/test1/doctype/test_2/test_2.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, test and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Test2(Document):
+	pass
