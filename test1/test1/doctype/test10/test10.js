d7e869163043e5319919045484f501464306f92e
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-01-24 19:11:38 +0530

    update

---
+++ test1/test1/doctype/test10/test10.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, test and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test10", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ test1/test1/doctype/test10/test_test10.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2025, test and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record depdendencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+class UnitTestTest10(UnitTestCase):
+	"""
+	Unit tests for Test10.
+	Use this class for testing individual functions and methods.
+	"""
+
+	pass
+
+
+class IntegrationTestTest10(IntegrationTestCase):
+	"""
+	Integration tests for Test10.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
